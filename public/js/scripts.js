fa65aebbd11e2c752a7aff54cebfaa88b2864e42
LucasKoval <lucaskoval@hotmail.com>
2021-03-27 03:02:09 -0300

    redeem interaction finished

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -10,3 +10,23 @@ function darkTheme() {
     const theme = document.querySelector(".theme");
     theme.classList.toggle("dark-mode");
 }
+
+// Redeem
+window.addEventListener("load",function(){
+    const card = document.querySelector(".card");
+    const redeem = document.querySelector(".redeem");
+    const shopIcon = document.querySelector("#shop-icon");
+    
+    card.addEventListener('mouseenter', () => {
+        console.log('mouseenter funciona');
+        redeem.classList.remove("visually-hidden");
+        shopIcon.src = '/images/icons/buy-white.svg'
+    });
+
+    card.addEventListener('mouseleave', () => {
+        console.log('mouseleave funciona');
+        redeem.classList.add("visually-hidden");
+        shopIcon.src = '/images/icons/buy-blue.svg'
+    });
+
+})
