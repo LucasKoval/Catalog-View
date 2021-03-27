9d2e4468073e1c5bb21aa4abe06a31ac66093324
LucasKoval <lucaskoval@hotmail.com>
2021-03-27 20:27:39 -0300

    Redeem overlay finished

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,7 +5,6 @@ function darkMode() {
     body.classList.toggle("dark-mode");
     theme.classList.toggle("dark-mode");
 }
-
 function darkTheme() {
     const theme = document.querySelector(".theme");
     theme.classList.toggle("dark-mode");
@@ -13,20 +12,21 @@ function darkTheme() {
 
 // Redeem
 window.addEventListener("load",function(){
-    const card = document.querySelector(".card");
-    const redeem = document.querySelector(".redeem");
-    const shopIcon = document.querySelector("#shop-icon");
-    
-    card.addEventListener('mouseenter', () => {
-        console.log('mouseenter funciona');
-        redeem.classList.remove("visually-hidden");
-        shopIcon.src = '/images/icons/buy-white.svg'
-    });
+    const card = document.querySelectorAll("div.card");
+    const redeem = document.querySelectorAll(".redeem");
+    const shopIcon = document.querySelectorAll(".shop-icon");
 
-    card.addEventListener('mouseleave', () => {
-        console.log('mouseleave funciona');
-        redeem.classList.add("visually-hidden");
-        shopIcon.src = '/images/icons/buy-blue.svg'
-    });
+    for (let i = 0; i < card.length; i++) {
+        card[i].addEventListener('mouseenter', () => {
+            redeem[i].classList.remove("visually-hidden");
+            card[i].classList.add("move-up");
+            shopIcon[i].src = '/images/icons/buy-white.svg'
+        });
 
-})
+        card[i].addEventListener('mouseleave', () => {
+            redeem[i].classList.add("visually-hidden");
+            card[i].classList.remove("move-up");
+            shopIcon[i].src = '/images/icons/buy-blue.svg'
+        });
+    }
+});
